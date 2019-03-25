40d369d44f79da8b3582d3a6bd03db1e80db674c
github username <github email address>
2019-03-25 14:00:17 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,16 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the dark side. Please type in a character's name."
+
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
+  input.downcase
+  # input.split(' ').map do |name|
+  #   name.downcase
+  # end.join(' ')
+
 end
